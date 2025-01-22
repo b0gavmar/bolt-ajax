866c3aff5d426439a872bf3d0723c0135109ad93
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-01-22 14:46:10 +0100

    bolt.json

---
+++ src/views/BoltView.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 class="text-center">cim</h1>
+</template>
---
+++ src/views/CartView.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="text-center">Kosár</h1>
+</template>
+
+<style scoped>
+
+</style>
+
+<script setup>
+
+</script>
