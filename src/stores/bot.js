6691b3cfc2ce166060a0c0e37e0d39aa238fb6c7
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-01-24 09:39:19 +0100

    addtocart

--- src/stores/bot.js
+++ src/stores/bot.js
@@ -2,18 +2,22 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useBotStore = defineStore('bot', () => {
-  const products = ref(0)
-  const cart = ref();
+  const products = ref([])
+  const cart = ref([]);
   /*const doubleCount = computed(() => count.value * 2)
   function increment() {
     count.value++
   }*/
 
-  const loadAll = () =>{
+  const loadAll = () => {
     fetch("http://localhost:3000/bolt")
     .then(resp => resp.json())
     .then(data => products.value = data)
   }
 
-  return { products, loadAll }
+  const addToCart = (id) => {
+    cart.value.push(products.value.find(p => p.id == id));
+  }
+
+  return { products, loadAll, addToCart }
 })
--- src/views/BoltView.vue
+++ src/views/BoltView.vue
@@ -8,10 +8,13 @@ const botStore = useBotStore();
 <template>
   <h1 class="text-center">Termékek</h1>
   <div class="row">
-    <div v-for="p in botStore.products" :key="p.id" class="card">
-      <p class="card-title"> {{ p.name }}</p>
-      <div class="card-footer">
-        <button class="btn btn-outline-primary">&#128722;</button>
+    <div v-for="p in botStore.products" :key="p.id" class="card col-12 col-md-4 col-lg-3">
+      <p class="card-title text-center"> {{ p.name }}</p>
+      <div class="card-body">
+        <p class="card-text">{{ p.price }}</p>
+      </div>
+      <div class="card-footer text-center">
+        <button class="btn btn-outline-primary w-50" @click="botStore.addToCart(p.id)">&#128722;</button>
       </div>
     </div>
   </div>
