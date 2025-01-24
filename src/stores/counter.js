a0b421fd7d041161bb16d2df334eaa64889d2e82
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-01-24 08:45:18 +0100

    dolgok

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,12 +1,19 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
+export const useBotStore = defineStore('products', () => {
+  const products = ref(0)
+  
+  /*const doubleCount = computed(() => count.value * 2)
   function increment() {
     count.value++
+  }*/
+
+  const loadAll = () =>{
+    fetch("http://localhost:3000/bolt")
+    .then(resp => resp.json())
+    .then(data => products.value = data)
   }
 
-  return { count, doubleCount, increment }
+  return { products, loadAll }
 })
