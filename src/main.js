96167378ef4e22462d7ab9389a51da706996c30e
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-02-05 15:00:01 +0100

    majdnem

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import 'vue-toastification/dist/index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { Toast } from 'vue-toastification'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Toast)
 
 app.mount('#app')
--- src/stores/bot.js
+++ src/stores/bot.js
@@ -1,9 +1,11 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { useToast } from 'vue-toastification';
 
 export const useBotStore = defineStore('bot', () => {
   const products = ref([])
-  const cart = ref({}); //elég tárolni a termék id-t és mennyiség párosát (key-value) (id-mennyiseg)
+  const cart = ref([]); //elég tárolni a termék id-t és mennyiség párosát (key-value) (id-mennyiseg)
+  const toast = useToast();
   /*const doubleCount = computed(() => count.value * 2)
   function increment() {
     count.value++
@@ -33,7 +35,10 @@ export const useBotStore = defineStore('bot', () => {
     //let id = Math.round(Math.random*100000000000)
     products.value.push(p)
     axios.post("http://localhost:3000/bolt",p)
-    .then(resp => console.log(resp.statusText))
+    .then(resp => {
+      console.log(resp.statusText)
+      Toast("Sikeres mentés")
+    }).catch(() => toast("Hiba"));
   }
 
   return { products, cart, loadAll, addToCart, saveProduct }
